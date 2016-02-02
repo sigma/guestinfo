84635653d0f859d9f7059c21dfe4223474fef29c
Yann Hodique <yann.hodique@gmail.com>
2016-02-01 20:41:03 -0800

    Revert "remove getVersion code as we now use bdoor"
    This reverts commit 77e6096fb6afd65de7b1dbe4ca68ef5e29d5394e.

--- guestinfo.go
+++ guestinfo.go
@@ -13,6 +13,29 @@ const (
 	backdoorCmdGetVersion = 10
 )
 
+type getVersionRequest struct {
+	registers
+}
+
+func newGetVersionRequest() getVersionRequest {
+	return getVersionRequest{
+		registers{
+			_ax: backdoorMagic,
+			_bx: 0xFFFFFFFF,
+			_cx: backdoorCmdGetVersion,
+			_dx: backdoorPort,
+		},
+	}
+}
+
+type getVersionResponse struct {
+	hvMessage
+}
+
+func (r getVersionResponse) isVMware() bool {
+	return r.hvMessage.bx() == backdoorMagic
+}
+
 func init() {
 	virtualWorld = bdoor.HypervisorPortCheck()
 }
